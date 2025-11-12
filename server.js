1fd7b6809dc856755f7fa3ef2f319760226744da
longonaga <longjohnonaga@gmail.com>
2025-11-12 09:16:33 +0100

    modified server.js ; removed useNewUrlparse and use Unified Topology options from the mongoose.connect method, as they are deprecated

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ async function start() {
   try {
     await mongoose.connect(process.env.MONGO_URL, {
       dbName:'EscrowaNG'
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
+      });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err.message);
